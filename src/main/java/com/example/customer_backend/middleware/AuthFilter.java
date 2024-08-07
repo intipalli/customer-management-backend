423af9ee0dd63efa38c927c77b86d316ce1d83cb
Mitchell <mbailey54@student.gsu.edu>
2024-08-07 13:33:06 +0000

    implemented authorization filter

---
+++ src/main/java/com/example/customer_backend/middleware/AuthFilter.java
@@ -0,0 +1,64 @@
+package com.example.customer_backend.middleware;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AuthFilter implements Filter{
+
+    //public static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+	private String auth_scope = "com.example.auth.apis";
+	private String api_scope = "com.example.data.apis";
+	
+    @Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// get authorization header
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String uri = req.getRequestURI();
+		
+		
+		// for development purposes
+		// allow turning off auth checking with header tokencheck:false
+		String tokenheader = req.getHeader("tokencheck");
+		if( tokenheader != null && !tokenheader.equalsIgnoreCase("true") ) {
+			chain.doFilter(request, response);
+			return;		
+		}
+		
+		// auth checking will not apply to these cases
+		// token endpoint
+		// user register endpoint
+		// healthcheck endpoint on '/api/'
+		if(   !uri.startsWith("/api/events") 
+	       && !uri.startsWith("/api/registrations")
+	       && !uri.equals("/api/customers")
+	       ) {
+			chain.doFilter(request, response);
+			return;			
+		}else{
+			// check JWT token
+			String authheader = req.getHeader("authorization");
+			if(authheader != null && authheader.length() > 7 && authheader.startsWith("Bearer")) {
+				String jwt_token = authheader.substring(7, authheader.length());
+				if(JWTFactory.verifyToken(jwt_token)) {
+					String request_scopes = JWTFactory.getScopes(jwt_token);
+					if(request_scopes.contains(api_scope) || request_scopes.contains(auth_scope)) {
+						chain.doFilter(request, response);
+						return;
+					}
+				}
+			}
+		}		
+		// continue
+		res.sendError(HttpServletResponse.SC_FORBIDDEN, "failed authentication");
+
+	}
+}
