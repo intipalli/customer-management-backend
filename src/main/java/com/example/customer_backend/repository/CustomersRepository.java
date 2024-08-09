5beb00320f6e7e416eb619c2940640f7757cf619
Rohan <rohan.intipalli@gmail.com>
2024-08-09 13:32:35 +0000

    enhanced customer crud apis

--- src/main/java/com/example/customer_backend/repository/CustomersRepository.java
+++ src/main/java/com/example/customer_backend/repository/CustomersRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.customer_backend.model.Customer;
 
 public interface CustomersRepository extends CrudRepository<Customer, Long> {
-
+    // Custom query method to find a customer by email
+    Customer findByEmail(String email);
 }
